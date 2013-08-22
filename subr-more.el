a1c9ae3573c42fafc460d87a7b5c5719bfb2fe66
Jerry James <loganjerry@gmail.com>
2013-08-22 15:59:30 -0600

    Add the functions number-sequence, string-or-null-p, and booleanp to subr-more.el for Emacs compatibility.  See xemacs-patches message <CAHCOHQ=SgzLmKr7+v6+G_DJJp0RJEKspGYJbPeqYJVxp3DvM7w@mail.gmail.com>.

--- subr-more.el
+++ subr-more.el
@@ -93,5 +93,57 @@ Same as `string-match' except this function does not change the match data."
 	 (res (string-match regexp string start buffer)))
     (store-match-data md)
     res))
-    
+
+;;;###autoload
+(defun number-sequence (from &optional to inc)
+  "Return a sequence of numbers from FROM to TO (both inclusive) as a list.
+INC is the increment used between numbers in the sequence and defaults to 1.
+So, the Nth element of the list is (+ FROM (* N INC)) where N counts from
+zero.  TO is only included if there is an N for which TO = FROM + N * INC.
+If TO is nil or numerically equal to FROM, return (FROM).
+If INC is positive and TO is less than FROM, or INC is negative
+and TO is larger than FROM, return nil.
+If INC is zero and TO is neither nil nor numerically equal to
+FROM, signal an error.
+
+This function is primarily designed for integer arguments.
+Nevertheless, FROM, TO and INC can be integer or float.  However,
+floating point arithmetic is inexact.  For instance, depending on
+the machine, it may quite well happen that
+\(number-sequence 0.4 0.6 0.2) returns the one element list (0.4),
+whereas (number-sequence 0.4 0.8 0.2) returns a list with three
+elements.  Thus, if some of the arguments are floats and one wants
+to make sure that TO is included, one may have to explicitly write
+TO as (+ FROM (* N INC)) or use a variable whose value was
+computed with this exact expression.  Alternatively, you can,
+of course, also replace TO with a slightly larger value
+\(or a slightly more negative value if INC is negative)."
+  (if (or (not to) (= from to))
+      (list from)
+    (or inc (setq inc 1))
+    (when (zerop inc) (error "The increment can not be zero"))
+    (let (seq (n 0) (next from))
+      (if (> inc 0)
+	  (while (<= next to)
+	    (setq seq (cons next seq)
+		  n (1+ n)
+		  next (+ from (* n inc))))
+	(while (>= next to)
+	  (setq seq (cons next seq)
+		n (1+ n)
+		next (+ from (* n inc)))))
+      (nreverse seq))))
+
+;;;###autoload
+(defun string-or-null-p (object)
+  "Return t if OBJECT is a string or nil.
+Otherwise, return nil."
+  (or (stringp object) (null object)))
+
+;;;###autoload
+(defun booleanp (object)
+  "Return t if OBJECT is one of the two canonical boolean values: t or nil.
+Otherwise, return nil."
+  (or (null object) (eq object t)))
+
 ;;; subr-more.el ends here
